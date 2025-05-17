2089bde644aed6ee55e56c87a9321cfd2533deeb
harshit2414 <harshitgarg9462@gmail.com>
2025-05-17 14:07:47 +0530

    Changes in UI

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -10,66 +10,59 @@ import { TbBulbFilled } from "react-icons/tb";
 
 const Navbar = ({ user, mode }) => {
   return (
-    <div>
-      <div className="large-screen">
-        <nav>
-          <div className="hamburger">
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-          </div>
-          <ul className="nav-links">
-            <li>
-              <img
-                className={`careloop-logo`}
-                src={"./images/sankalpLogo.png"}
-                alt="CareLoop Logo"
-              />
-            </li>
-            <li>
-              <Link to="/home" className="mainLinks">
-                Home
-              </Link>
-              <FaHome />
-            </li>
-            <li>
-              <Link to="/therapy-chatbot" className="mainLinks">
-                Mia
-              </Link>
-              <RiPsychotherapyLine />
-            </li>
-            <li>
-              <Link to="/story" className="mainLinks">
-                Stories
-              </Link>
-              <TbBulbFilled />
-            </li>
-            <li>
-              <Link to="/contact-us" className="mainLinks">
-                Contact
-              </Link>
-              <FaEnvelope />
-            </li>
-            <li>
-              <Link to="/profile" className="mainLinks">
-                Profile
-              </Link>
-              <FaUserAlt />
-            </li>
-            <li>
-              <img
-                className={`profile-pic-navbar-${mode}`}
-                src={
-                  !user || user?.profilepic === ""
-                    ? `./images/anonymousProfilePic${mode}.jpg`
-                    : user?.profilepic
-                }
-                alt={"Profile Pic"}
-              />
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="navbar-wrapper">
+      <nav>
+        <div className="hamburger">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <img
+              className="sankalp-logo"
+              src="./images/careloopLogo.png"
+              alt="Sankalp Logo"
+            />
+          </li>
+          <li>
+            <Link to="/home" className="mainLinks">
+              Home <FaHome />
+            </Link>
+          </li>
+          <li>
+            <Link to="/therapy-chatbot" className="mainLinks">
+              Mia <RiPsychotherapyLine />
+            </Link>
+          </li>
+          <li>
+            <Link to="/story" className="mainLinks">
+              Stories <TbBulbFilled />
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact-us" className="mainLinks">
+              Contact <FaEnvelope />
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" className="mainLinks">
+              Profile <FaUserAlt />
+            </Link>
+          </li>
+          <li>
+            <img
+              className={`profile-pic-navbar-${mode}`}
+              src={
+                !user || user?.profilepic === ""
+                  ? `./images/anonymousProfilePic${mode}.jpg`
+                  : user?.profilepic
+              }
+              alt="Profile Pic"
+            />
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
